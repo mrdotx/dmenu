072c285699f131b62e013e030e1de396ddd73eb8
mrdotx <klassiker@gmx.de>
2020-05-21 10:13:00 +0200

    small changes and symbols only removed

--- scripts/dmenu_unicode_symbols.sh
+++ scripts/dmenu_unicode_symbols.sh
@@ -3,21 +3,23 @@
 # path:       /home/klassiker/.local/share/repos/dmenu/scripts/dmenu_unicode_symbols.sh
 # author:     klassiker [mrdotx]
 # github:     https://github.com/mrdotx/dmenu
-# date:       2020-05-19T20:39:40+0200
+# date:       2020-05-21T10:02:40+0200
 
 # get active window id
 win_id=$(xprop -root | awk '/_NET_ACTIVE_WINDOW\(WINDOW\)/{print $NF}')
 
-# symbols file
-# chosen=$(< "$HOME/.local/share/repos/dmenu/scripts/data/symbols-unicode" dmenu -l 15 -r -i -p "symbol:" -w "$win_id")
-chosen=$(< "$HOME/.local/share/repos/dmenu/scripts/data/symbols" dmenu -b -l 15 -r -i -p "symbol:" -w "$win_id")
-[ -n "$chosen" ] || exit
+sel=$(< "$HOME/.local/share/repos/dmenu/scripts/data/symbols-unicode" dmenu -b -l 15 -r -i -p "symbol:" -w "$win_id")
 
+[ -n "$sel" ] || exit
+
+symb=$(printf "%s\n" "$sel" | sed "s/ .*//")
+code=$(printf "%s\n" "$sel" | cut -d ';' -f2)
+
+# insert to cursor in active window
+xdotool type "$symb"
 # copy symbol to clipboard
-clip=$(printf "%s\n" "$chosen" | sed "s/ .*//")
-printf "%s\n" "$clip" | tr -d '\n' | xsel -b
+printf "%s\n" "$symb" | tr -d '\n' | xsel -b
 # copy code to primary
-pri=$(printf "%s\n" "$chosen" | sed "s/.*; //" | awk '{print $1}')
-printf "%s\n" "$pri" | tr -d '\n' | xsel
+printf "%s\n" "$code" | tr -d '\n' | xsel
 
-notify-send "clipboard" "Copied to clipboard\t: $clip\nCopied to primary\t: $pri"
+notify-send "copied to clipboard" "clipboard: $symb\nprimary: $code"
