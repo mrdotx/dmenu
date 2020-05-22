41a032cc45d2aae98d3c63bb6fa5fa6d3d311f08
mrdotx <klassiker@gmx.de>
2020-05-22 18:27:12 +0200

    colors changed

--- config.def.h
+++ config.def.h
@@ -19,7 +19,7 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeNorm] = { "#cccccc", "#000000" },
 	[SchemeSel] = { "#cccccc", "#4185d7" },
 	[SchemeSelHighlight] = { "#ffffff", "#4185d7" },
-	[SchemeNormHighlight] = { "#3476a4", "#000000" },
+	[SchemeNormHighlight] = { "#4185d7", "#000000" },
 	[SchemeOut] = { "#000000", "#009698" },
 };
 static const unsigned int alphas[SchemeLast][2] = {
