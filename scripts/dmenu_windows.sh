243a0c407cc4c1663d401c1c644e14baad3fadff
mrdotx <klassiker@gmx.de>
2020-05-24 01:03:40 +0200

    desktop number integrated

--- scripts/dmenu_windows.sh
+++ scripts/dmenu_windows.sh
@@ -3,20 +3,29 @@
 # path:       /home/klassiker/.local/share/repos/dmenu/scripts/dmenu_windows.sh
 # author:     klassiker [mrdotx]
 # github:     https://github.com/mrdotx/dmenu
-# date:       2020-05-23T20:56:07+0200
+# date:       2020-05-24T00:58:24+0200
 
-num=$(wmctrl -l \
-    | sed 's/  / /' \
-    | cut -d " " -f4- \
+desk=$(mktemp "/tmp/.dmenu_windows.XXXXXX")
+win=$(mktemp "/tmp/.dmenu_windows.XXXXXX")
+
+wmctrl -d > "$desk"
+wmctrl -l > "$win"
+
+idx=$(awk 'FNR==NR{a[$1]=$9 ;next}{ print "\["a[$2]"\] - ", $0 }' "$desk" "$win" 2>/dev/null \
+    | cut -d " " -f1-2,8- \
     | nl -w 3 -n rn \
     | sed -r 's/^([ 0-9]+)[ \t]*(.*)$/\1 - \2/' \
-    | dmenu -l 10 -c -bw 2 -i -p "window:" \
-    | cut -d '-' -f-1\
+    | dmenu -l 10 -c -bw 2 -r -i -p "window:" \
+    | cut -d '-' -f-1 \
 )
 
-[ -n "$num" ] || exit
+[ -z "$idx" ] \
+    && rm -f "$desk" "$win" \
+    && exit
 
 wmctrl -l \
-    | sed -n "$num p" \
+    | sed -n "$idx p" \
     | cut -c -10 \
     | xargs wmctrl -i -a
+
+rm -f "$desk" "$win"
