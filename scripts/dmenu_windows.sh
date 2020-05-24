a0f6059fa6c05983b71f80f8710afe7b26f23df3
mrdotx <klassiker@gmx.de>
2020-05-24 21:03:04 +0200

    opening method completed

--- scripts/dmenu_windows.sh
+++ scripts/dmenu_windows.sh
@@ -3,7 +3,7 @@
 # path:       /home/klassiker/.local/share/repos/dmenu/scripts/dmenu_windows.sh
 # author:     klassiker [mrdotx]
 # github:     https://github.com/mrdotx/dmenu
-# date:       2020-05-24T09:39:17+0200
+# date:       2020-05-24T21:00:03+0200
 
 desk=$(mktemp "/tmp/dmenu_windows.XXXXXX")
 win=$(mktemp "/tmp/dmenu_windows.XXXXXX")
@@ -11,7 +11,7 @@ win=$(mktemp "/tmp/dmenu_windows.XXXXXX")
 wmctrl -d > "$desk"
 wmctrl -l > "$win"
 
-idx=$(awk 'FNR==NR{a[$1]=$9 ;next}{ print "\["a[$2]"\] - ", $0 }' "$desk" "$win" 2>/dev/null \
+idx=$(awk 'FNR==NR{a[$1]=$9 ;next}{ print "\["a[$2]"\] » ", $0 }' "$desk" "$win" 2>/dev/null \
     | cut -d " " -f1-2,8- \
     | nl -w 3 -n rn \
     | sed -r 's/^([ 0-9]+)[ \t]*(.*)$/\1 - \2/' \
@@ -23,8 +23,7 @@ idx=$(awk 'FNR==NR{a[$1]=$9 ;next}{ print "\["a[$2]"\] - ", $0 }' "$desk" "$win"
     && rm -f "$desk" "$win" \
     && exit
 
-wmctrl -l \
-    | sed -n "$idx p" \
+sed -n "$idx p" "$win" \
     | cut -c -10 \
     | xargs wmctrl -i -a
 
