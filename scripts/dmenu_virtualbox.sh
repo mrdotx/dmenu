3f815b72673d2c03103c85cef38292296377060f
mrdotx <klassiker@gmx.de>
2020-05-24 10:39:20 +0200

    optimized

--- scripts/dmenu_virtualbox.sh
+++ scripts/dmenu_virtualbox.sh
@@ -3,11 +3,10 @@
 # path:       /home/klassiker/.local/share/repos/dmenu/scripts/dmenu_virtualbox.sh
 # author:     klassiker [mrdotx]
 # github:     https://github.com/mrdotx/dmenu
-# date:       2020-05-23T19:15:28+0200
+# date:       2020-05-24T10:22:08+0200
 
 sel=$(VBoxManage list vms \
-    | cut -d ' ' -f1 \
-    | sed 's/\"//g' \
+    | cut -d '"' -f2 \
     | dmenu -l 5 -c -bw 2 -r -i -p "vm:" \
 )
 
