268ad4d1ecb6655ebbea76a7714418cea87dbe3a
mrdotx <klassiker@gmx.de>
2020-05-27 17:34:36 +0200

    already connected implemented

--- scripts/dmenu_iwd.sh
+++ scripts/dmenu_iwd.sh
@@ -3,7 +3,7 @@
 # path:       /home/klassiker/.local/share/repos/dmenu/scripts/dmenu_iwd.sh
 # author:     klassiker [mrdotx]
 # github:     https://github.com/mrdotx/dmenu
-# date:       2020-05-27T12:11:41+0200
+# date:       2020-05-27T17:23:54+0200
 
 script=$(basename "$0")
 help="$script [-h/--help] -- script to connect to wlan with iwd
@@ -43,39 +43,43 @@ case $script in
         ;;
 esac
 
-cln_iwctl(){
+cln_iwd(){
     tail -n +5 \
         | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g;/^\s*$/d"
 }
 
 get_ifc(){
     ifc=$(iwctl device list \
-        | cln_iwctl \
+        | cln_iwd \
         | awk '{print $1" == ["$2"] == ["$3"]"}' \
         | $menu_ifc -p "$label_ifc" \
         | awk '{print $1}'
     )
+    [ -n "$ifc" ] \
+        || exit 1
 }
 
 scan_ssid(){
     iwctl station "$ifc" scan && sleep 1
     scan_res=$(iwctl station "$ifc" get-networks \
-        | cln_iwctl \
+        | cln_iwd \
         | sed 's/ psk / ; [psk ] ; /;s/ open / ; [open] ; /;s/\s\+/ /g' \
         | awk -F " ; " '{print $2" =="$1}' \
     )
 }
 
 get_ssid(){
-    [ -n "$ifc" ] \
-        || exit 1
     sel=$(printf "[scan] == rescan?\n%s" "$scan_res" \
         | $menu_ssid -p "$label_ssid" \
     )
     ssid=$(printf "%s" "$sel" \
         | awk -F" == " '{print $2}' \
-        | sed 's/> //' \
     )
+    if printf "%s" "$ssid" | grep -q "^> "; then
+        notify-send "iNet wireless daemon" "already connected to \"$(printf "%s" "$ssid" \
+            | sed 's/> //')\""
+        exit 0
+    fi
     [ "$(printf "%s" "$sel" \
         | awk -F" == " '{print $1}')" = "[open]" ] \
         && open=1
@@ -93,14 +97,17 @@ get_psk(){
     )
 }
 
+conn_iwd(){
+    if [ -z "$open" ]; then
+        get_psk
+        iwctl station "$ifc" connect "$ssid" -P "$psk"
+    else
+        iwctl station "$ifc" connect "$ssid"
+    fi
+    notify-send "iNet wireless daemon" "connected to \"$ssid\""
+}
+
 get_ifc \
     && scan_ssid \
-    && get_ssid
-if [ -z "$open" ]; then
-    get_psk
-    iwctl station "$ifc" connect "$ssid" -P "$psk" \
-    && notify-send "connected to $ssid"
-else
-    iwctl station "$ifc" connect "$ssid" \
-    && notify-send "connected to $ssid"
-fi
+    && get_ssid \
+    && conn_iwd
