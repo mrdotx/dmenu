f264f77a776f6a43e907f0ea6e3856dc9ed42393
mrdotx <klassiker@gmx.de>
2020-07-05 12:51:07 +0200

    sync changed from brave to firefox-developer-edition

--- scripts/dmenu_bookmarks.sh
+++ scripts/dmenu_bookmarks.sh
@@ -3,7 +3,7 @@
 # path:       /home/klassiker/.local/share/repos/dmenu/scripts/dmenu_bookmarks.sh
 # author:     klassiker [mrdotx]
 # github:     https://github.com/mrdotx/dmenu
-# date:       2020-06-08T08:31:42+0200
+# date:       2020-07-05T11:58:04+0200
 
 script=$(basename "$0")
 help="$script [-h/--help] -- script to open bookmarks with dmenu/rofi
@@ -58,23 +58,49 @@ select=$(printf "%s" "$title" \
 # open bookmark
 case "$open" in
     bookmarks_sync)
-        # copy bookmarks from firefox to surf
-        # printf 'select url from moz_bookmarks, moz_places where moz_places.id=moz_bookmarks.fk;\n' \
-        #     | sqlite3 ~/.mozilla/firefox/*.default-*/places.sqlite \
-        #     | awk -F '//' '{print $2}' \
-        #     | sed '/^$/d' \
-        #     | sort > ~/.config/surf/bookmarks
+        brave_to_surf() {
+            grep \"url\": ~/.config/BraveSoftware/Brave-Browser/Default/Bookmarks \
+                | awk -F '"' '{print $4}' \
+                | awk -F '//' '{print $2}' \
+                | sort > ~/.config/surf/bookmarks
+        }
 
-        # copy bookmarks from brave to surf
-        grep \"url\": ~/.config/BraveSoftware/Brave-Browser/Default/Bookmarks \
-            | awk -F '"' '{print $4}' \
-            | awk -F '//' '{print $2}' \
-            | sort > ~/.config/surf/bookmarks
+        brave_to_qutebrowser() {
+            grep \"url\": ~/.config/BraveSoftware/Brave-Browser/Default/Bookmarks \
+                | awk -F '"' '{print $4}' \
+                | sort > ~/.config/qutebrowser/bookmarks/urls
+        }
 
-        # copy bookmarks from brave to qutebrowser
-        grep \"url\": ~/.config/BraveSoftware/Brave-Browser/Default/Bookmarks \
-            | awk -F '"' '{print $4}' \
-            | sort > ~/.config/qutebrowser/bookmarks/urls
+        firefox_close() {
+            killall -q /usr/lib/firefox-developer-edition/firefox \
+                && firefox=1 \
+                && sleep 0.1
+        }
+
+        firefox_to_surf() {
+            printf 'select url from moz_bookmarks, moz_places where moz_places.id=moz_bookmarks.fk;\n' \
+                | sqlite3 ~/.mozilla/firefox/*-default*/places.sqlite \
+                | awk -F '//' '{print $2}' \
+                | sed '/^$/d' \
+                | sort > ~/.config/surf/bookmarks
+        }
+
+        firefox_to_qutebrowser() {
+            printf 'select url from moz_bookmarks, moz_places where moz_places.id=moz_bookmarks.fk;\n' \
+                | sqlite3 ~/.mozilla/firefox/*-default*/places.sqlite \
+                | sort > ~/.config/qutebrowser/bookmarks/urls
+        }
+
+        # firefox functions
+        firefox_close
+        firefox_to_surf
+        firefox_to_qutebrowser
+        [ $firefox = 1 ] \
+            && firefox-developer-edition &
+
+        # brave functions
+        # brave_to_surf
+        # brave_to_qutebrowser
 
         notify-send "bookmarks" "synchronized"
         dmenu_bookmarks.sh
