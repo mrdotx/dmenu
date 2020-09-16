f7fc9c62c1f829725c36c16ecb3cd34da3036d3c
mrdotx <klassiker@gmx.de>
2020-09-16 13:55:59 +0200

    option to clear clipboard added

--- scripts/dmenu_youtube.sh
+++ scripts/dmenu_youtube.sh
@@ -3,7 +3,7 @@
 # path:       /home/klassiker/.local/share/repos/dmenu/scripts/dmenu_youtube.sh
 # author:     klassiker [mrdotx]
 # github:     https://github.com/mrdotx/dmenu
-# date:       2020-09-16T13:30:29+0200
+# date:       2020-09-16T13:53:18+0200
 
 script=$(basename "$0")
 help="$script [-h/--help] -- script to search youtube with youtube-dl and play
@@ -41,13 +41,22 @@ case $script in
 esac
 
 search_results=10
-search=$(printf "%s" "$(xsel -o -b)" \
+
+[ -n "$(xsel -o -b)" ] \
+    && search=$(printf "== clear clipboard ==\n%s" "$(xsel -o -b)")
+
+search=$(printf "%s" "$search" \
     | $menu -p "$label")
 
 [ -z "$search" ] \
     && exit 1
 
 case "$search" in
+    "== clear clipboard ==")
+        xsel -c -b
+        $script
+        exit 0
+        ;;
     *'youtube.com/watch'* \
         | *'youtube.com/playlist'* \
         | *'youtu.be'* \
