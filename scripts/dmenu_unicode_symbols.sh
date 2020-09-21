20f609d33466a17ae03724f58d1058184fdfc971
mrdotx <klassiker@gmx.de>
2020-09-21 19:11:04 +0200

    small structure update

--- scripts/dmenu_unicode_symbols.sh
+++ scripts/dmenu_unicode_symbols.sh
@@ -3,7 +3,7 @@
 # path:       /home/klassiker/.local/share/repos/dmenu/scripts/dmenu_unicode_symbols.sh
 # author:     klassiker [mrdotx]
 # github:     https://github.com/mrdotx/dmenu
-# date:       2020-09-19T19:41:23+0200
+# date:       2020-09-21T17:47:41+0200
 
 script=$(basename "$0")
 help="$script [-h/--help] -- script to search and copy unicode symbols
@@ -39,9 +39,12 @@ case $script in
         ;;
 esac
 
-select=$(< "$HOME/.local/share/repos/dmenu/scripts/data/unicode-symbols" $menu -p "$label")
+select=$(< "$HOME/.local/share/repos/dmenu/scripts/data/unicode-symbols" \
+    $menu -p "$label" \
+)
 
-[ -n "$select" ] || exit 1
+[ -n "$select" ] \
+    || exit 1
 
 symbol=$(printf "%s\n" "$select" \
     | sed 's/ .*//' \
