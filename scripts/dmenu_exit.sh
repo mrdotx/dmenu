addfeb1791ce76ff730245c9a2bb49abed2ae7dc
mrdotx <klassiker@gmx.de>
2020-10-25 18:48:11 +0100

    position from center to top changed

--- scripts/dmenu_exit.sh
+++ scripts/dmenu_exit.sh
@@ -3,7 +3,7 @@
 # path:       /home/klassiker/.local/share/repos/dmenu/scripts/dmenu_exit.sh
 # author:     klassiker [mrdotx]
 # github:     https://github.com/mrdotx/dmenu
-# date:       2020-10-19T19:53:07+0200
+# date:       2020-10-25T18:45:10+0100
 
 script=$(basename "$0")
 help="$script [-h/--help] -- script to shutdown/reboot/logout/suspend/lock
@@ -25,11 +25,11 @@ fi
 case $script in
     dmenu_*)
         label="exit »"
-        menu="dmenu -m 0 -l 8 -c -bw 2 -r -i"
+        menu="dmenu -m 0 -r -i"
         ;;
     rofi_*)
         label=""
-        menu="rofi -m -1 -l 4 -columns 2 -theme klassiker-center -dmenu -i"
+        menu="rofi -m -1 -theme klassiker-horizontal -dmenu -i"
         ;;
     *)
         printf "%s\n" "$help"
