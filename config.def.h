c39e63b14f4f809987097da1dbb32af930dcb5c1
mrdotx <klassiker@gmx.de>
2021-06-14 21:16:33 +0200

    patch xresources replaces with xresources alt

--- config.def.h
+++ config.def.h
@@ -1,34 +1,53 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom */
-static int centered = 0;                    /* -c option; centers dmenu on screen */
-static int min_width = 250;                 /* minimum width when centered */
+/* -b  option; if 0, dmenu appears at bottom */
+static int topbar = 1;
+/* -c option; centers dmenu on screen */
+static int centered = 0;
+/* minimum width when centered */
+static int min_width = 250;
 /* -fn option overrides fonts[0]; default X11 font or font set */
+static char font[] = "DejaVuSansMono Nerd Font:pixelsize=16:antialias=true:autohint=true";
+static char fontfallback[] = "Source-Code-Pro:pixelsize=14:antialias=true:autohint=true";
+static char fontemoji[] = "JoyPixels:pixelsize=14:antialias=true:autohint=true";
 static const char *fonts[] = {
-    "DejaVuSansMono Nerd Font:pixelsize=16:antialias=true:autohint=true",
-    "Source-Code-Pro:pixelsize=14:antialias=true:autohint=true",
-    "JoyPixels:pixelsize=14:antialias=true:autohint=true"
+	font,
+    fontfallback,
+    fontemoji
 };
 static const unsigned int alpha = 0xe6;
-static const char *prompt      = "run »";      /* -p  option; prompt to the left of input field */
+/* -p  option; prompt to the left of input field */
+static const char *prompt = "run »";
 static const char *symbol_1 = "«";
 static const char *symbol_2 = "»";
-static const char *colors[SchemeLast][2] = {
-/*                  foreground, background */
-    [SchemeNorm] = { "#cccccc", "#000000" },
-    [SchemeSel] = { "#cccccc", "#4185d7" },
-    [SchemeSelHighlight] = { "#ffffff", "#4185d7" },
-    [SchemeNormHighlight] = { "#4185d7", "#000000" },
-    [SchemeOut] = { "#000000", "#1f5393" },
+
+static char foreground[]              = "#cccccc";
+static char background[]              = "#000000";
+static char selforeground[]           = "#cccccc";
+static char selbackground[]           = "#4185d7";
+static char selhighlightforeground[]  = "#ffffff";
+static char selhighlightbackground[]  = "#4185d7";
+static char normhighlightforeground[] = "#4185d7";
+static char normhighlightbackground[] = "#000000";
+static char outforeground[]           = "#000000";
+static char outbackground[]           = "#1f5393";
+static char *colors[SchemeLast][2] = {
+	/*                    foreground, background */
+	[SchemeNorm]          = { foreground, background },
+	[SchemeSel]           = { selforeground, selbackground },
+	[SchemeSelHighlight]  = { selhighlightforeground, selhighlightbackground },
+	[SchemeNormHighlight] = { normhighlightforeground, normhighlightbackground },
+	[SchemeOut]           = { outforeground, outbackground },
 };
+
 static const unsigned int alphas[SchemeLast][2] = {
-/*              foreground, background */
-    [SchemeNorm] = { OPAQUE, alpha },
-    [SchemeSel] = { OPAQUE, OPAQUE },
-    [SchemeSelHighlight] = { OPAQUE, OPAQUE },
+    /*                    foreground, background */
+    [SchemeNorm]          = { OPAQUE, alpha },
+    [SchemeSel]           = { OPAQUE, OPAQUE },
+    [SchemeSelHighlight]  = { OPAQUE, OPAQUE },
     [SchemeNormHighlight] = { OPAQUE, alpha },
-    [SchemeOut] = { OPAQUE, OPAQUE },
+    [SchemeOut]           = { OPAQUE, OPAQUE },
 };
 
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
@@ -45,3 +64,22 @@ static const char worddelimiters[] = " ";
 
 /* Size of the window border */
 static unsigned int border_width = 0;
+
+/*
+ * Xresources preferences to load at startup
+ */
+ResourcePref resources[] = {
+	{ "font",                    STRING, &font },
+	{ "fontfallback",            STRING, &fontfallback },
+	{ "fontemoji",               STRING, &fontemoji },
+	{ "foreground",              STRING, &foreground },
+	{ "background",              STRING, &background },
+	{ "selforeground",           STRING, &selforeground },
+	{ "selbackground",           STRING, &selbackground },
+	{ "selhighlightforeground",  STRING, &selhighlightforeground },
+	{ "selhighlightbackground",  STRING, &selhighlightbackground },
+	{ "normhighlightforeground", STRING, &normhighlightforeground },
+	{ "normhighlightbackground", STRING, &normhighlightbackground },
+	{ "outforeground",           STRING, &outforeground },
+	{ "outbackground",           STRING, &outbackground },
+};
--- dmenu.c
+++ dmenu.c
@@ -12,11 +12,11 @@
 #include <X11/Xatom.h>
 #include <X11/Xproto.h>
 #include <X11/Xutil.h>
+#include <X11/Xresource.h>
 #ifdef XINERAMA
 #include <X11/extensions/Xinerama.h>
 #endif
 #include <X11/Xft/Xft.h>
-#include <X11/Xresource.h>
 
 #include "drw.h"
 #include "util.h"
@@ -66,9 +66,20 @@ static Visual *visual;
 static int depth;
 static Colormap cmap;
 
-/* Temporary arrays to allow overriding xresources values */
-static char *colortemp[4];
-static char *tempfonts;
+/* Xresources preferences */
+enum resource_type {
+	STRING = 0,
+	INTEGER = 1,
+	FLOAT = 2
+};
+typedef struct {
+	char *name;
+	enum resource_type type;
+	void *dst;
+} ResourcePref;
+
+static void load_xresources(void);
+static void resource_load(XrmDatabase db, char *name, enum resource_type rtype, void *dst);
 
 #include "config.h"
 
@@ -492,7 +503,7 @@ keypress(XKeyEvent *ev)
 
 	switch(ksym) {
 	default:
-insert:
+	insert:
 		if (!iscntrl(*buf))
 			insert(buf, len);
 		break;
@@ -757,6 +768,54 @@ readstdin(void)
 	lines = MIN(lines, i);
 }
 
+void
+resource_load(XrmDatabase db, char *name, enum resource_type rtype, void *dst)
+{
+	char *sdst = NULL;
+	int *idst = NULL;
+	float *fdst = NULL;
+	sdst = dst;
+	idst = dst;
+	fdst = dst;
+	char fullname[256];
+	char *type;
+	XrmValue ret;
+	snprintf(fullname, sizeof(fullname), "%s.%s", "dmenu", name);
+	fullname[sizeof(fullname) - 1] = '\0';
+	XrmGetResource(db, fullname, "*", &type, &ret);
+	if (!(ret.addr == NULL || strncmp("String", type, 64)))
+	{
+		switch (rtype) {
+		case STRING:
+			strcpy(sdst, ret.addr);
+			break;
+		case INTEGER:
+			*idst = strtoul(ret.addr, NULL, 10);
+			break;
+		case FLOAT:
+			*fdst = strtof(ret.addr, NULL);
+			break;
+		}
+	}
+}
+
+void
+load_xresources(void)
+{
+	Display *display;
+	char *resm;
+	XrmDatabase db;
+	ResourcePref *p;
+	display = XOpenDisplay(NULL);
+	resm = XResourceManagerString(display);
+	if (!resm)
+		return;
+	db = XrmGetStringDatabase(resm);
+	for (p = resources; p < resources + LENGTH(resources); p++)
+		resource_load(db, p->name, p->type, p->dst);
+	XCloseDisplay(display);
+}
+
 static void
 run(void)
 {
@@ -814,13 +873,8 @@ setup(void)
 	int a, di, n, area = 0;
 #endif
 	/* init appearance */
-	for (j = 0; j < SchemeLast; j++) {
-		scheme[j] = drw_scm_create(drw, (const char**)colors[j], alphas[j], 2);
-	}
-	for (j = 0; j < SchemeOut; ++j) {
-		for (i = 0; i < 2; ++i)
-			free(strdup(colors[j][i]));
-	}
+	for (j = 0; j < SchemeLast; j++)
+		scheme[j] = drw_scm_create(drw, colors[j], alphas[j], 2);
 
 	clip = XInternAtom(dpy, "CLIPBOARD",   False);
 	utf8 = XInternAtom(dpy, "UTF8_STRING", False);
@@ -932,79 +986,14 @@ usage(void)
 	exit(1);
 }
 
-void
-readxresources(void) {
-	XrmInitialize();
-
-	char* xrm;
-	if ((xrm = XResourceManagerString(drw->dpy))) {
-		char *type;
-		XrmDatabase xdb = XrmGetStringDatabase(xrm);
-		XrmValue xval;
-
-		if (XrmGetResource(xdb, "dmenu.font", "*", &type, &xval))
-			fonts[0] = strdup(xval.addr);
-		else
-			fonts[0] = strdup(fonts[0]);
-        if (XrmGetResource(xdb, "dmenu.fontfallback", "*", &type, &xval))
-			fonts[1] = strdup(xval.addr);
-		else
-			fonts[1] = strdup(fonts[1]);
-        if (XrmGetResource(xdb, "dmenu.fontemoji", "*", &type, &xval))
-			fonts[2] = strdup(xval.addr);
-		else
-			fonts[2] = strdup(fonts[2]);
-        if (XrmGetResource(xdb, "dmenu.background", "*", &type, &xval))
-			colors[SchemeNorm][ColBg] = strdup(xval.addr);
-		else
-			colors[SchemeNorm][ColBg] = strdup(colors[SchemeNorm][ColBg]);
-
-        if (XrmGetResource(xdb, "dmenu.foreground", "*", &type, &xval))
-			colors[SchemeNorm][ColFg] = strdup(xval.addr);
-		else
-			colors[SchemeNorm][ColFg] = strdup(colors[SchemeNorm][ColFg]);
-        if (XrmGetResource(xdb, "dmenu.selbackground", "*", &type, &xval))
-			colors[SchemeSel][ColBg] = strdup(xval.addr);
-		else
-			colors[SchemeSel][ColBg] = strdup(colors[SchemeSel][ColBg]);
-        if (XrmGetResource(xdb, "dmenu.selforeground", "*", &type, &xval))
-			colors[SchemeSel][ColFg] = strdup(xval.addr);
-		else
-			colors[SchemeSel][ColFg] = strdup(colors[SchemeSel][ColFg]);
-        if (XrmGetResource(xdb, "dmenu.selhighlightbackground", "*", &type, &xval))
-			colors[SchemeSelHighlight][ColBg] = strdup(xval.addr);
-		else
-			colors[SchemeSelHighlight][ColBg] = strdup(colors[SchemeSelHighlight][ColBg]);
-        if (XrmGetResource(xdb, "dmenu.selhighlightforeground", "*", &type, &xval))
-			colors[SchemeSelHighlight][ColFg] = strdup(xval.addr);
-		else
-			colors[SchemeSelHighlight][ColFg] = strdup(colors[SchemeSelHighlight][ColFg]);
-        if (XrmGetResource(xdb, "dmenu.normhighlightbackground", "*", &type, &xval))
-			colors[SchemeNormHighlight][ColBg] = strdup(xval.addr);
-		else
-			colors[SchemeNormHighlight][ColBg] = strdup(colors[SchemeNormHighlight][ColBg]);
-        if (XrmGetResource(xdb, "dmenu.normhighlightforeground", "*", &type, &xval))
-			colors[SchemeNormHighlight][ColFg] = strdup(xval.addr);
-		else
-			colors[SchemeNormHighlight][ColFg] = strdup(colors[SchemeNormHighlight][ColFg]);
-        if (XrmGetResource(xdb, "dmenu.outbackground", "*", &type, &xval))
-			colors[SchemeOut][ColBg] = strdup(xval.addr);
-		else
-			colors[SchemeOut][ColBg] = strdup(colors[SchemeOut][ColBg]);
-        if (XrmGetResource(xdb, "dmenu.outforeground", "*", &type, &xval))
-			colors[SchemeOut][ColFg] = strdup(xval.addr);
-		else
-			colors[SchemeOut][ColFg] = strdup(colors[SchemeOut][ColFg]);
-
-		XrmDestroyDatabase(xdb);
-	}
-}
-
 int
 main(int argc, char *argv[])
 {
 	XWindowAttributes wa;
 	int i, fast = 0;
+
+	XrmInitialize();
+	load_xresources();
 
 	for (i = 1; i < argc; i++)
 		/* these options take no arguments */
@@ -1036,15 +1025,15 @@ main(int argc, char *argv[])
 		else if (!strcmp(argv[i], "-p"))   /* adds prompt to left of input field */
 			prompt = argv[++i];
 		else if (!strcmp(argv[i], "-fn"))  /* font or font set */
-			tempfonts = argv[++i];
+			fonts[0] = argv[++i];
 		else if (!strcmp(argv[i], "-nb"))  /* normal background color */
-			colortemp[0] = argv[++i];
+			colors[SchemeNorm][ColBg] = argv[++i];
 		else if (!strcmp(argv[i], "-nf"))  /* normal foreground color */
-			colortemp[1] = argv[++i];
+			colors[SchemeNorm][ColFg] = argv[++i];
 		else if (!strcmp(argv[i], "-sb"))  /* selected background color */
-			colortemp[2] = argv[++i];
+			colors[SchemeSel][ColBg] = argv[++i];
 		else if (!strcmp(argv[i], "-sf"))  /* selected foreground color */
-			colortemp[3] = argv[++i];
+			colors[SchemeSel][ColFg] = argv[++i];
 		else if (!strcmp(argv[i], "-w"))   /* embedding window id */
 			embed = argv[++i];
 		else if (!strcmp(argv[i], "-bw"))
@@ -1065,23 +1054,8 @@ main(int argc, char *argv[])
 		    parentwin);
 	xinitvisual();
 	drw = drw_create(dpy, screen, root, wa.width, wa.height, visual, depth, cmap);
-	readxresources();
-	/* Now we check whether to override xresources with commandline parameters */
-	if ( tempfonts )
-	   fonts[0] = strdup(tempfonts);
-	if ( colortemp[0])
-	   colors[SchemeNorm][ColBg] = strdup(colortemp[0]);
-	if ( colortemp[1])
-	   colors[SchemeNorm][ColFg] = strdup(colortemp[1]);
-	if ( colortemp[2])
-	   colors[SchemeSel][ColBg]  = strdup(colortemp[2]);
-	if ( colortemp[3])
-	   colors[SchemeSel][ColFg]  = strdup(colortemp[3]);
-
-	if (!drw_fontset_create(drw, (const char**)fonts, LENGTH(fonts)))
+	if (!drw_fontset_create(drw, fonts, LENGTH(fonts)))
 		die("no fonts could be loaded.");
-
-	free(strdup(fonts[0]));
 	lrpad = drw->fonts->h;
 
 #ifdef __OpenBSD__
