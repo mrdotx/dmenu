ef5548dc309eb2d932473d5d976fe42f0043e1dc
mrdotx <klassiker@gmx.de>
2021-06-19 18:13:12 +0200

    added calculation to label

--- scripts/dmenu_calc.sh
+++ scripts/dmenu_calc.sh
@@ -3,7 +3,7 @@
 # path:   /home/klassiker/.local/share/repos/dmenu/scripts/dmenu_calc.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/dmenu
-# date:   2021-06-16T17:40:07+0200
+# date:   2021-06-19T18:08:50+0200
 
 # get active window id
 window_id=$(xprop -root \
@@ -15,11 +15,17 @@ result=$(printf "%s\n" "$@" \
     | bc -l \
     | sed '/\./ s/\.\{0,1\}0\{1,\}$//' \
 )
+
+# set result to label
+[ -n "$result" ] \
+    && label="$* = $result"
+
 select=$(printf "%s\n" \
             "clear" \
             "copy to clipboard" \
-    | dmenu -b -l 3 -w "$window_id" -p "= $result" \
+    | dmenu -b -l 3 -w "$window_id" -p "${label-"calc"}" \
 )
+
 case $select in
     "")
         ;;
@@ -34,6 +40,6 @@ case $select in
                 "Result copied: $result"
         ;;
     *)
-        "$0" "$result $select"
+        "$0" "$result$select"
         ;;
 esac
