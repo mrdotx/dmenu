b742db73ab797de30877ba7c9aa8ff09bd115dd6
mrdotx <klassiker@gmx.de>
2021-08-30 19:27:44 +0200

    mechanism for default settings changed and some minor optimizations

--- scripts/dmenu_display.sh
+++ scripts/dmenu_display.sh
@@ -3,7 +3,13 @@
 # path:   /home/klassiker/.local/share/repos/dmenu/scripts/dmenu_display.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/dmenu
-# date:   2021-07-15T12:53:20+0200
+# date:   2021-08-30T19:26:16+0200
+
+# config
+screen_layouts_file="$HOME/.local/share/repos/dmenu/scripts/data/screen-layouts"
+primary="HDMI2"
+secondary="eDP1"
+scale_dimensions="1.0x1.0"
 
 all_displays=$(xrandr \
     | grep "connected" \
@@ -14,11 +20,31 @@ connected_displays=$(printf "%s" "$all_displays" \
 )
 
 default_settings() {
-    select=$(screenlayout.sh --defaults \
+    select=$(printf "%s" "$(cat "$screen_layouts_file")" \
         | dmenu -l 10 -c -bw 2 -i -p "$select »" \
     )
     [ -n "$select" ] \
-        && screenlayout.sh "$select"
+        && pri_mode=$(printf "%s" "$select" | cut -d ';' -f1) \
+        && pri_pos=$(printf "%s" "$select" | cut -d ';' -f2) \
+        && pri_rate=$(printf "%s" "$select" | cut -d ';' -f3) \
+        && pri_rotate=$(printf "%s" "$select" | cut -d ';' -f4) \
+        && sec_mode=$(printf "%s" "$select" | cut -d ';' -f5) \
+        && sec_pos=$(printf "%s" "$select" | cut -d ';' -f6) \
+        && sec_rate=$(printf "%s" "$select" | cut -d ';' -f7) \
+        && sec_rotate=$(printf "%s" "$select" | cut -d ';' -f8) \
+        && xrandr \
+            --output "$primary" --auto --primary \
+            --mode "${pri_mode:-1920x1080}" \
+            --pos "${pri_pos:-0x0}" \
+            --rate "${pri_rate:-60}" \
+            --rotate "${pri_rotate:-normal}" \
+            --scale "$scale_dimensions" \
+            --output "$secondary" --auto \
+            --mode "${sec_mode:-1920x1080}" \
+            --pos "${sec_pos:-1920x0}" \
+            --rate "${sec_rate:-60}" \
+            --rotate "${sec_rotate:-normal}" \
+            --scale "$scale_dimensions"
 }
 
 refresh_rate() {
@@ -76,6 +102,31 @@ rotate() {
         --rotate "$direction"
 }
 
+extend() {
+    primary=$(printf "%s" "$connected_displays" \
+        | dmenu -l 4 -c -bw 2 -r -i -p "primary »" \
+    )
+    [ -z "$primary" ] \
+        && exit 0
+    secondary=$(printf "%s" "$connected_displays" \
+        | grep -v "$primary" \
+        | head -n1 \
+    )
+
+    orientation=$(printf "above\nright\nbelow\nleft" \
+        | dmenu -l 4 -c -bw 2 -r -i -p "position of $secondary »" \
+        | sed 's/left/left-of/;s/right/right-of/' \
+    )
+    [ -z "$orientation" ] \
+        && exit 0
+
+    xrandr \
+        --output "$primary" --auto --primary \
+        --scale "$scale_dimensions" \
+        --output "$secondary" --"$orientation" "$primary" --auto \
+        --scale "$scale_dimensions"
+}
+
 mirror() {
     external=$(printf "%s" "$connected_displays" \
         | dmenu -l 4 -c -bw 2 -r -i -p "resolution from »" \
@@ -119,36 +170,10 @@ mirror() {
 
     xrandr \
         --output "$external" --auto \
-        --scale 1.0x1.0 \
+        --scale "$scale_dimensions" \
         --output "$internal" --auto \
         --same-as "$external" \
         --scale "$scale_x"x"$scale_y"
-}
-
-
-extend() {
-    primary=$(printf "%s" "$connected_displays" \
-        | dmenu -l 4 -c -bw 2 -r -i -p "primary »" \
-    )
-    [ -z "$primary" ] \
-        && exit 0
-    secondary=$(printf "%s" "$connected_displays" \
-        | grep -v "$primary" \
-        | head -n1 \
-    )
-
-    orientation=$(printf "above\nright\nbelow\nleft" \
-        | dmenu -l 4 -c -bw 2 -r -i -p "position of $secondary »" \
-        | sed 's/left/left-of/;s/right/right-of/' \
-    )
-    [ -z "$orientation" ] \
-        && exit 0
-
-    xrandr \
-        --output "$primary" --auto --primary \
-        --scale 1.0x1.0 \
-        --output "$secondary" --"$orientation" "$primary" --auto \
-        --scale 1.0x1.0
 }
 
 # menu
@@ -184,7 +209,7 @@ select=$(printf "%s\n" \
     *)
         eval xrandr \
             --output "$select" --auto --primary \
-            --scale 1.0x1.0 \
+            --scale "$scale_dimensions" \
             "$(printf "%s" "$all_displays" \
                 | grep -v "$select" \
                 | awk '{print "--output", $1, "--off"}' \
