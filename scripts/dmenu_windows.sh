802134222b03fea06922f8a88c4d0e78d5d1b5a9
mrdotx <klassiker@gmx.de>
2021-10-01 09:35:07 +0200

    minor fixes

--- scripts/dmenu_windows.sh
+++ scripts/dmenu_windows.sh
@@ -3,7 +3,7 @@
 # path:   /home/klassiker/.local/share/repos/dmenu/scripts/dmenu_windows.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/dmenu
-# date:   2021-07-15T12:45:04+0200
+# date:   2021-10-01T08:43:51+0200
 
 desktops=$(mktemp -t dmenu_windows.XXXXXX)
 windows=$(mktemp -t dmenu_windows.XXXXXX)
@@ -11,12 +11,12 @@ windows=$(mktemp -t dmenu_windows.XXXXXX)
 wmctrl -d > "$desktops"
 wmctrl -l > "$windows"
 
-select=$(awk 'FNR==NR{a[$1]=$2" ["$9 ;next}{ print a[$2]"] » ", $0 }' "$desktops" "$windows" \
-    | cut -d " " -f1-2,9- \
+select=$(awk 'FNR==NR{a[$1]=$2" ["$9 ;next}{ print a[$2]"] ", $0 }' "$desktops" "$windows" \
+    | cut -d " " -f1-2,8- \
     | nl -w 2 -n rz \
     | sed -r 's/^([ 0-9]+)[\t]*(.*)$/\1 \2/' \
     | dmenu -l 15 -c -bw 2 -r -i -p "window »" \
-    | cut -d '-' -f-1 \
+    | cut -d ' ' -f1 \
 )
 
 if [ -n "$select" ]; then
