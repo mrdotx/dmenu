eaf0d97405956df9833b671696d2e3a9eea189b8
mrdotx <klassiker@gmx.de>
2021-10-02 15:35:31 +0200

    menu changed

--- scripts/dmenu_ssh.sh
+++ scripts/dmenu_ssh.sh
@@ -3,17 +3,17 @@
 # path:   /home/klassiker/.local/share/repos/dmenu/scripts/dmenu_ssh.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/dmenu
-# date:   2021-08-31T18:45:27+0200
+# date:   2021-10-02T15:25:01+0200
 
 # config
 ssh_config="$HOME/.ssh/config"
 edit="$TERMINAL -e $EDITOR"
 
 select=$(printf "%s\n" \
+    "== edit config ==" \
     "$(grep "^Host " "$ssh_config" \
         | cut -d ' ' -f2)" \
-    "== edit config ==" \
-    | dmenu -p "ssh »")
+    | dmenu -l 10 -c -bw 2 -i -p "ssh »")
 
 case "$select" in
     "== edit config ==")
