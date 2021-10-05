4edb1b9e7eac585e144eb08af4bcf0968a9e522a
mrdotx <klassiker@gmx.de>
2021-10-05 22:24:51 +0200

    dmenu position changed to active window

--- scripts/dmenu_screenshot.sh
+++ scripts/dmenu_screenshot.sh
@@ -3,13 +3,16 @@
 # path:   /home/klassiker/.local/share/repos/dmenu/scripts/dmenu_screenshot.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/dmenu
-# date:   2021-07-16T19:50:54+0200
+# date:   2021-10-05T22:17:40+0200
 
 # config
 screenshot_directory="$HOME/Desktop"
 screenshot_file="$screenshot_directory/screenshot-$(date +"%FT%T%z").png"
 screenshot_command="maim -B -u -q $screenshot_file"
 screenshot_preview="sxiv $screenshot_file"
+
+# get active window id
+window_id=$(xdotool getactivewindow)
 
 execute() {
     screenshot_command="$screenshot_command $*"
@@ -24,7 +27,7 @@ select=$(printf "%s\n" \
     "selection" \
     "desktop --delay 5" \
     "window --delay 5" \
-    | dmenu -l 5 -c -bw 2 -p "screenshot »"
+    | dmenu -b -l 5 -w "$window_id" -p "screenshot »"
 )
 
 case $select in
