9bedc7cfe92e082f3b4647dc46a6da10cc22750d
mrdotx <klassiker@gmx.de>
2021-10-05 18:54:06 +0200

    open editor moved to command line option

--- scripts/dmenu_vim.sh
+++ scripts/dmenu_vim.sh
@@ -3,50 +3,53 @@
 # path:   /home/klassiker/.local/share/repos/dmenu/scripts/dmenu_vim.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/dmenu
-# date:   2021-07-15T17:57:14+0200
+# date:   2021-10-05T18:44:50+0200
 
 open() {
-    $TERMINAL -e vim "$1://$2/" -c ":call NetrwToggle()"
+    $TERMINAL -e "$EDITOR" "$1://$2/" -c ":call NetrwToggle()"
 }
 
-# menu for vim shortcuts
-case $(printf "%s\n" \
-    "== ideas ==" \
-    "== notes ==" \
-    "== new ==" \
-    "pi" \
-    "pi2" \
-    "middlefinger" \
-    "prinzipal" \
-    "klassiker" \
-    "marcus" \
-    | dmenu -l 9 -c -bw 2 -r -i -p "vim »" \
-    ) in
-    "== ideas ==")
-        $TERMINAL -e vim -c ":VimwikiIndex" -c ":VimwikiGoto ideas"
+case "$1" in
+    --editor)
+        $TERMINAL -e "$EDITOR"
         ;;
-    "== notes ==")
-        $TERMINAL -e vim -c ":VimwikiIndex"
-        ;;
-    "== new ==")
-        $TERMINAL -e vim
-        ;;
-    "pi")
-        open "scp" "hermes"
-        ;;
-    "pi2")
-        open "scp" "prometheus"
-        ;;
-    "middlefinger")
-        open "scp" "middlefinger"
-        ;;
-    "prinzipal")
-        open "scp" "prinzipal"
-        ;;
-    "klassiker")
-        open "ftp" "klassiker.online.de"
-        ;;
-    "marcus")
-        open "ftp" "marcusreith.de"
+    *)
+        # menu for editor shortcuts
+        case $(printf "%s\n" \
+            "== ideas ==" \
+            "== notes ==" \
+            "pi" \
+            "pi2" \
+            "middlefinger" \
+            "prinzipal" \
+            "klassiker" \
+            "marcus" \
+            | dmenu -l 9 -c -bw 2 -r -i -p "vim »" \
+            ) in
+            "== ideas ==")
+                $TERMINAL -e "$EDITOR" -c ":VimwikiIndex" -c ":VimwikiGoto ideas"
+                ;;
+            "== notes ==")
+                $TERMINAL -e "$EDITOR" -c ":VimwikiIndex"
+                ;;
+            "pi")
+                open "scp" "hermes"
+                ;;
+            "pi2")
+                open "scp" "prometheus"
+                ;;
+            "middlefinger")
+                open "scp" "middlefinger"
+                ;;
+            "prinzipal")
+                open "scp" "prinzipal"
+                ;;
+            "klassiker")
+                open "ftp" "klassiker.online.de"
+                ;;
+            "marcus")
+                open "ftp" "marcusreith.de"
+                ;;
+        esac
         ;;
 esac
