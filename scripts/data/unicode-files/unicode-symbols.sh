8d7acde3ccfa1d1d0f82c7e63d992e9648728b5d
mrdotx <klassiker@gmx.de>
2021-12-16 19:39:25 +0100

    initial upload

---
+++ scripts/data/unicode-files/unicode-symbols.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+# path:   /home/klassiker/.local/share/repos/dmenu/scripts/data/unicode-files/unicode-symbols.sh
+# author: klassiker [mrdotx]
+# github: https://github.com/mrdotx/dmenu
+# date:   2021-12-16T19:38:21+0100
+
+output_file="../unicode-symbols"
+
+# emoji downloaded from
+# https://unicode.org/Public/emoji/14.0/emoji-test.txt
+emoji_file="emoji-test.txt"
+
+# font awesome copied from
+# https://fontawesome.com/v5/cheatsheet
+fa_files=$(find . -iname "fa-*")
+
+# others
+others_file="others.txt"
+
+# emoji
+grep "; fully-qualified" "$emoji_file" \
+    | awk -F "; fully-qualified     # " '{print $2 "; " $1}' \
+    | sed 's/[ \t]*$//' \
+    | cut -d' ' -f2 --complement \
+    | sort -u -k 2 > "$output_file"
+
+# font awesome
+for file in "$fa_files"; do
+    sed '/^$/d' $file \
+        | awk 'ORS=NR%3?FS:RS' \
+        | awk -F "     " '{print $1 " fa-" $2 "; " $3}'
+done \
+    | sort -u -k 2 >> "$output_file"
+
+# others
+cat "$others_file" \
+    | sort -u -k 2 >> "$output_file"
