0cdaaea2353d9911261f257032aa5d677410a92c
mrdotx <klassiker@gmx.de>
2022-04-17 16:27:24 +0200

    volume controls moved to i3

--- scripts/dmenu_alsa.sh
+++ scripts/dmenu_alsa.sh
@@ -3,75 +3,15 @@
 # path:   /home/klassiker/.local/share/repos/dmenu/scripts/dmenu_alsa.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/dmenu
-# date:   2022-04-03T17:15:59+0200
+# date:   2022-04-17T16:05:31+0200
 
-# speed up script by not using unicode
+# don't use unicode to identify the playback device
 LC_ALL=C
 LANG=C
 
 # config
 config_path="$HOME/.config/alsa"
 config_file="asoundrc"
-analog_filter="analog"
-message_title="Volume"
-
-script=$(basename "$0")
-help="$script [-h/--help] -- script to change alsa audio output
-  Usage:
-    $script [-inc/-dec/-abs/-mute] [percent]
-
-  Settings:
-    [-inc]    = increase in percent (0-100)
-    [-dec]    = decrease in percent (0-100)
-    [-abs]    = absolute volume in percent (0-100)
-    [percent] = how much percent to increase/decrease the volume
-    [-mute]   = mute volume
-
-  Examples:
-    $script -inc 5
-    $script -dec 5
-    $script -abs 36
-    $script -mute"
-
-notification() {
-    volume="$(amixer get "$1" \
-        | tail -1 \
-        | cut -d'[' -f2 \
-        | sed 's/%]*//' \
-    )"
-
-    if amixer get "$2" | tail -1 | grep "\[off\]" >/dev/null; then
-        volume=0 \
-        volume_indicator="MUTE"
-    else
-        volume=$((volume /= ${3:-1}))
-        volume=$((volume *= ${3:-1}))
-        volume_indicator="$volume"
-    fi
-
-    notify-send \
-        -u low  \
-        -t 2000 \
-        -i "dialog-information" \
-        "$message_title $volume_indicator" \
-        -h string:x-canonical-private-synchronous:"$message_title" \
-        -h int:value:"$volume"
-}
-
-get_analog() {
-    card=$(grep -m1 "card" "$config_path/$config_file" \
-        | sed 's/^    //' \
-    )
-
-    if aplay -l \
-        | grep -m1 -i -e "^$card.*$analog_filter" > /dev/null 2>&1; then
-        device_mixer="Master"
-        device_mute="Master"
-    else
-        device_mixer="PCM"
-        device_mute="IEC958"
-    fi
-}
 
 set_asoundrc() {
     mkdir -p "$config_path"
@@ -98,46 +38,10 @@ set_asoundrc() {
         "}" > "$config_path/$config_file"
 }
 
-set_volume() {
-    if [ "$#" -eq 2 ] \
-        && [ "$2" -ge 0 ] > /dev/null 2>&1 \
-        && [ "$2" -le 100 ] > /dev/null 2>&1; then
-            amixer -q set $device_mixer "$2%$1"
-            notification "$device_mixer" "$device_mute" "$2"
-    else
-        printf "%s\n" "$help"
-        exit 1
-    fi
-}
+select=$(aplay -l \
+    | grep '^card' \
+    | dmenu -l 10 -c -bw 2 -r -i -p "device »" \
+)
 
-case "$1" in
-    -h | --help)
-        printf "%s\n" "$help"
-        ;;
-    -inc)
-        get_analog
-        set_volume "+" "$2"
-        ;;
-    -dec)
-        get_analog
-        set_volume "-" "$2"
-        ;;
-    -abs)
-        get_analog
-        set_volume "" "$2"
-        ;;
-    -mute)
-        get_analog
-        amixer -q set $device_mute toggle
-        notification "$device_mixer" "$device_mute"
-        ;;
-    *)
-        select=$(aplay -l \
-            | grep '^card' \
-            | dmenu -l 10 -c -bw 2 -r -i -p "device »" \
-        )
-
-        [ -n "$select" ] \
-            && set_asoundrc "$select"
-        ;;
-esac
+[ -n "$select" ] \
+    && set_asoundrc "$select"
