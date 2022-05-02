2d9c89ce447c134412187a4bfd0fb3012454a47f
mrdotx <klassiker@gmx.de>
2022-05-02 09:58:26 +0200

    nerd fonts added

--- scripts/data/unicode-files/unicode-symbols.sh
+++ scripts/data/unicode-files/unicode-symbols.sh
@@ -3,35 +3,66 @@
 # path:   /home/klassiker/.local/share/repos/dmenu/scripts/data/unicode-files/unicode-symbols.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/dmenu
-# date:   2022-02-03T18:40:03+0100
+# date:   2022-05-02T09:54:57+0200
 
 output_file="../unicode-symbols"
 
-# emoji downloaded from
-# https://unicode.org/Public/emoji/14.0/emoji-test.txt
-emoji_file="emoji-test.txt"
+get_nerdfont() {
+    nerdfonts_url="https://www.nerdfonts.com/cheat-sheet"
 
-# font awesome copied from
-# https://fontawesome.com/v5/cheatsheet
-fa_files=$(find . -iname "fa-*")
+    get_char() {
+        hex="\u$1"
+        env printf '%b' "$hex"
+    }
 
-# others
-others_file="others.txt"
+    data=$(curl -fsS "$nerdfonts_url" \
+        | grep "class-name" \
+        | sed \
+            -e 's/    <div class="class-name">/;/g' \
+            -e 's/<div class="codepoint">/;/g' \
+            -e 's/<\/div>//g' \
+    )
 
-# emoji
-grep "; fully-qualified" "$emoji_file" \
-    | awk -F "; fully-qualified     # " '{print $2 "; " $1}' \
-    | sed 's/[ \t]*$//' \
-    | cut -d' ' -f2 --complement \
-    | sort -u -k 2 > "$output_file"
+    for line in $data; do
+        class=$(printf "%s" "$line" | cut -d';' -f2)
+        hex=$(printf "%s" "$line" | cut -d';' -f3)
+        printf "%s %s; %s\n" "$(get_char "$hex")" "$class" "$hex"
+    done
+}
 
-# font awesome
-for file in $fa_files; do
-    sed '/^$/d' "$file" \
-        | awk 'ORS=NR%3?FS:RS' \
-        | awk -F "     " '{print $1 " fa-" $2 "; " $3}'
-done \
-    | sort -u -k 2 >> "$output_file"
+get_fontawesome() {
+    # font awesome copied from
+    # https://fontawesome.com/v5/cheatsheet
+    fa_files=$(find . -iname "fa-*")
 
-# others
-    sort -u -k 2 "$others_file" >> "$output_file"
+    for file in $fa_files; do
+        sed '/^$/d' "$file" \
+            | awk 'ORS=NR%3?FS:RS' \
+            | awk -F "     " '{print $1 " fa-" $2 "; " $3}'
+    done
+}
+
+get_emoji() {
+    emoji_url="https://unicode.org/Public/emoji/14.0/emoji-test.txt"
+
+    curl -fsS "$emoji_url" \
+        | grep "; fully-qualified" \
+        | awk -F "; fully-qualified     # " '{print $2 "; " $1}' \
+        | sed 's/[ \t]*$//' \
+        | cut -d' ' -f2 --complement
+}
+
+    # others
+    sort -u -k 2 "others.txt" > "$output_file"
+
+# write symbols to file
+{
+    # nerd font
+    get_nerdfont | sort -u -k 2;
+
+    # font awesome
+    get_fontawesome | sort -u -k 2;
+
+    # emoji
+    get_emoji | sort -u -k 2
+} >> "$output_file"
