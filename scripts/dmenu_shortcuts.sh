a40d06fdb6b9c4b46786c1b6324325d30b48b97a
mrdotx <klassiker@gmx.de>
2022-05-14 12:10:56 +0200

    minor structural changes

--- scripts/dmenu_shortcuts.sh
+++ scripts/dmenu_shortcuts.sh
@@ -3,7 +3,7 @@
 # path:   /home/klassiker/.local/share/repos/dmenu/scripts/dmenu_shortcuts.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/dmenu
-# date:   2022-05-14T07:51:26+0200
+# date:   2022-05-14T12:03:32+0200
 
 shortcuts_file="$HOME/.local/share/repos/dmenu/scripts/data/shortcuts"
 
@@ -16,13 +16,12 @@ get_column() {
 select=$(dmenu -b -l 15 -bw 1 -r -i -p "shortcut »" < "$shortcuts_file")
 
 [ -n "$select" ] \
-    && select=$( \
-        printf "%s\n%s\n%s\n" \
-                "$(get_column 3)" \
-                "$(get_column 2)" \
-                "$(get_column 1)" \
-            | dmenu -b -l 3 -bw 1 -r -i -p "shortcut »" \
-            | sed -e "s/^ //" -e "s/\(\/\|\[\|\]\)/./g" \
+    && select=$(printf "%s\n%s\n%s\n" \
+                    "$(get_column 3)" \
+                    "$(get_column 2)" \
+                    "$(get_column 1)" \
+                | dmenu -b -l 3 -bw 1 -r -i -p "shortcut »" \
+                | sed -e "s/^ //" -e "s/\(\/\|\[\|\]\)/./g" \
     )
 
 [ -n "$select" ] \
