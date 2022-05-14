198863981370b0f64376bac70fa8df3b29985ff6
mrdotx <klassiker@gmx.de>
2022-05-14 12:10:44 +0200

    minor changes

--- scripts/dmenu_ssh.sh
+++ scripts/dmenu_ssh.sh
@@ -3,17 +3,18 @@
 # path:   /home/klassiker/.local/share/repos/dmenu/scripts/dmenu_ssh.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/dmenu
-# date:   2022-04-27T09:46:38+0200
+# date:   2022-05-14T12:05:57+0200
 
 # config
 ssh_config="$HOME/.ssh/config"
 edit="$TERMINAL -e $EDITOR"
 
 select=$(printf "%s\n" \
-    "== edit config ==" \
-    "$(grep "^Host " "$ssh_config" \
-        | cut -d ' ' -f2)" \
-    | dmenu -l 10 -c -bw 1 -i -p "ssh »")
+            "== edit config ==" \
+            "$(grep "^Host " "$ssh_config" \
+                | cut -d ' ' -f2)" \
+            | dmenu -l 10 -c -bw 1 -i -p "ssh »" \
+)
 
 case "$select" in
     "== edit config ==")
