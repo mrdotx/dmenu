a453e7cd339cc89e01dd98d9a97155a6084a91fb
mrdotx <klassiker@gmx.de>
2022-05-14 12:09:43 +0200

    structural optimizations

--- scripts/dmenu_windows.sh
+++ scripts/dmenu_windows.sh
@@ -3,7 +3,7 @@
 # path:   /home/klassiker/.local/share/repos/dmenu/scripts/dmenu_windows.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/dmenu
-# date:   2022-04-27T09:46:55+0200
+# date:   2022-05-14T12:09:29+0200
 
 desktops=$(mktemp -t dmenu_windows.XXXXXX)
 windows=$(mktemp -t dmenu_windows.XXXXXX)
@@ -11,21 +11,18 @@ windows=$(mktemp -t dmenu_windows.XXXXXX)
 wmctrl -d > "$desktops"
 wmctrl -l > "$windows"
 
-select=$( \
-    awk 'FNR==NR{a[$1]=$2" ["$9;next}{print a[$2]"]",$0}' \
-        "$desktops" "$windows" \
-    | cut -d ' ' -f1-2,7- \
-    | nl -w 2 -n rz -s ' ' \
-    | dmenu -l 15 -c -bw 1 -r -i -p 'window »' \
-    | cut -d ' ' -f1 \
+select=$(awk 'FNR==NR{a[$1]=$2" ["$9;next}{print a[$2]"]",$0}' \
+            "$desktops" "$windows" \
+                | cut -d ' ' -f1-2,7- \
+                | nl -w 2 -n rz -s ' ' \
+                | dmenu -l 15 -c -bw 1 -r -i -p 'window »' \
+                | cut -d ' ' -f1 \
 )
 
-if [ -n "$select" ]; then
-    wmctrl -i -a "$( \
+[ -n "$select" ] \
+    && wmctrl -i -a "$( \
         sed -n "$select p" "$windows" \
-        | cut -c -10 \
+            | cut -c -10 \
     )"
-    rm -f "$desktops" "$windows"
-else
-    rm -f "$desktops" "$windows"
-fi
+
+rm -f "$desktops" "$windows"
