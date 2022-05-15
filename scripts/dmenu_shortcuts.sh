d0fead36cbfe031fa762c59c235e678596f2108d
mrdotx <klassiker@gmx.de>
2022-05-15 16:14:35 +0200

    menu position changed to center

--- scripts/dmenu_shortcuts.sh
+++ scripts/dmenu_shortcuts.sh
@@ -3,7 +3,7 @@
 # path:   /home/klassiker/.local/share/repos/dmenu/scripts/dmenu_shortcuts.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/dmenu
-# date:   2022-05-14T12:38:12+0200
+# date:   2022-05-15T16:14:00+0200
 
 shortcuts_file="$HOME/.local/share/repos/dmenu/scripts/data/shortcuts"
 
@@ -13,14 +13,14 @@ get_column() {
         | sed -e 's/^ *//' -e 's/ *$//'
 }
 
-select=$(dmenu -b -l 15 -bw 1 -r -i -p "shortcut »" < "$shortcuts_file")
+select=$(dmenu -c -l 15 -bw 1 -r -i -p "shortcut »" < "$shortcuts_file")
 
 [ -n "$select" ] \
     && select=$(printf "%s\n%s\n%s\n" \
                     "$(get_column 3 "$select")" \
                     "$(get_column 2 "$select")" \
                     "$(get_column 1 "$select")" \
-                | dmenu -b -l 3 -bw 1 -r -i -p "shortcut »" \
+                | dmenu -c -l 3 -bw 1 -r -i -p "shortcut »" \
                 | sed -e "s/^ //" -e "s/\(\/\|\[\|\]\)/./g" \
     )
 
