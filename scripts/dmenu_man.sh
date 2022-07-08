69f65ae588842c7a753d553f3659f3c0031ee095
mrdotx <klassiker@gmx.de>
2022-07-08 19:41:00 +0200

    apropos removed

--- scripts/dmenu_man.sh
+++ scripts/dmenu_man.sh
@@ -3,9 +3,9 @@
 # path:   /home/klassiker/.local/share/repos/dmenu/scripts/dmenu_man.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/dmenu
-# date:   2022-05-14T12:06:51+0200
+# date:   2022-07-08T19:14:52+0200
 
-select=$(apropos -l '' \
+select=$(man -k -l '' \
     | sort \
     | dmenu -b -l 15 -bw 1 -r -i -p "man »" \
     | cut -d ' ' -f1,2 \
