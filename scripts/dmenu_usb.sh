0dc1f523a2538a73fca1e36b0111a112de0e1168
mrdotx <klassiker@gmx.de>
2022-07-09 20:22:18 +0200

    logical devices added

--- scripts/dmenu_usb.sh
+++ scripts/dmenu_usb.sh
@@ -3,27 +3,61 @@
 # path:   /home/klassiker/.local/share/repos/dmenu/scripts/dmenu_usb.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/dmenu
-# date:   2022-07-07T12:04:49+0200
+# date:   2022-07-09T20:11:59+0200
 
 # auth can be something like sudo -A, doas -- or nothing,
 # depending on configuration requirements
 auth="${EXEC_AS_USER:-sudo}"
+path="/sys/bus/usb/drivers/usb"
 
-usb_bind() {
-    $auth sh -c "printf '%s' \"$1\" \
-        > \"/sys/bus/usb/drivers/usb/$2\"" 2>/dev/null
+get_devices() {
+    logical_devices() {
+        ports=$(find "$path" -type l -name '[0-9]*-[0-9]*' \
+            | sort \
+            | cut -d '/' -f7 \
+        )
+
+        for port in $ports; do
+            get_info() {
+                [ -e "$path/$port/$1" ] \
+                    && cat "$path/$port/$1"
+            }
+
+            printf "%s: %s %s\n" \
+                "$port" \
+                "$(get_info "manufacturer")" \
+                "$(get_info "product")" \
+                    | awk '{$1=$1;print}'
+        done
+    }
+
+    root_hubs() {
+        lsusb \
+            | grep "Device 001" \
+            | sort -k 2,4 \
+            | sed -e 's/Bus 00/usb/g' \
+                -e 's/Bus 0/usb/g' \
+                -e 's/ Device [0-9][0-9][0-9]//g' \
+                -e 's/ ID ....:....//g'
+    }
+
+    printf "%s\n" \
+        "$(logical_devices)" \
+        "$(root_hubs)"
 }
 
 usb_bus() {
+    usb_bind() {
+        $auth sh -c "printf '%s' \"$1\" > \"$path/$2\"" 2>/dev/null
+    }
+
     [ -n "$2" ] \
-        && select=$(lsusb \
+        && select=$(get_devices \
             | grep -m1 "$2" \
-            | sed -e 's/Bus 00/usb/g' \
-                -e 's/Bus 0/usb/g' \
-            | cut -d ' ' -f1 \
+            | cut -d ':' -f1 \
         )
 
-    case $1 in
+    case "$1" in
         rebind)
             usb_bind "$select" "unbind"
             sleep 1
@@ -39,22 +73,14 @@ usb_bus() {
 }
 
 select_usb() {
-    select=$(lsusb \
-        | sort -k 2,4 \
-        | sed -e 's/Bus 00/usb/g' \
-            -e 's/Bus 0/usb/g' \
-            -e 's/ Device [0-9][0-9][0-9]//g' \
-            -e 's/ ID ....:....//g' \
+    select=$(get_devices \
         | dmenu -l 15 -c -bw 1 -r -i -p "usb »" \
         | cut -d ':' -f1 \
     )
 
     [ -n "$select" ] \
-        && bind=$(printf "%s\n" \
-                "bind" \
-                "unbind" \
-                "rebind" \
-                    | dmenu -l 3 -c -bw 1 -r -i -p "usb »" \
+        && bind=$(printf "bind\nunbind\nrebind" \
+            | dmenu -l 3 -c -bw 1 -r -i -p "$select »" \
         )
 
     [ -n "$bind" ] \
@@ -62,7 +88,7 @@ select_usb() {
 }
 
 # command line options bind/unbind/rebind
-# e.g. dmenu_usb.sh --unbind "Wacom Co., Ltd CTH-480"
+# e.g. dmenu_usb.sh --unbind "^usb3:"
 case "$1" in
     --*)
         usb_bus "${1##*--}" "$2"
