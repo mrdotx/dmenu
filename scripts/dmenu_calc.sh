33a27febf82e0e5c7e2d4b19b17b204dc46b8f2e
mrdotx <klassiker@gmx.de>
2023-01-31 13:47:30 +0100

    menu changed, result format optimized and power costs added

--- scripts/dmenu_calc.sh
+++ scripts/dmenu_calc.sh
@@ -3,7 +3,7 @@
 # path:   /home/klassiker/.local/share/repos/dmenu/scripts/dmenu_calc.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/dmenu
-# date:   2022-06-21T20:28:06+0200
+# date:   2023-01-31T13:43:25+0100
 
 # get active window id
 window_id=$(xdotool getactivewindow)
@@ -13,23 +13,29 @@ clipboard=$(xsel -n -o -b)
 
 # use bc for calculations
 result=$(printf "%s\n" "$@" \
-    | bc -l \
-    | sed '/\./ s/\.\{0,1\}0\{1,\}$//' \
+    | bc -l
 )
 
-# set result to label
+# format result and label
 [ -n "$result" ] \
+    && result=$(printf "%.4f\n" "$result") \
     && label="$* = $result »"
 
-menu=$(printf "%s\n" \
+menu() {
+    [ -n "$*" ] \
+        && printf "%s\n" \
+            "$*" \
             "clear" \
             "insert at cursor" \
-            "copy to clipboard" \
-            "clear clipboard" \
-            "$clipboard")
+            "copy to clipboard"
+    printf "%s\n" \
+        "power costs" \
+        "clear clipboard" \
+        "$clipboard"
+}
 
-select=$(printf "%s\n" "$menu" \
-    | dmenu -b -l 5 -bw 1 -w "$window_id" -p "${label-"calc »"}" \
+select=$(printf "%s\n" "$(menu "$*")" \
+    | dmenu -b -l 6 -bw 1 -w "$window_id" -p "${label-"calc »"}" \
 )
 
 case $select in
@@ -59,6 +65,9 @@ case $select in
                 "Clipboard" \
                 "cleared..." \
             && "$0" &
+        ;;
+    "power costs")
+        "$0" "24*7*52/1000*0.40" &
         ;;
     [-+/*]*)
         "$0" "$result$select" &
