0dcaae4fd0200cf4505773e8e06c0b0208aeff90
mrdotx <klassiker@gmx.de>
2023-06-08 07:40:58 +0200

    decimal point replaced by comma

--- scripts/dmenu_calc.sh
+++ scripts/dmenu_calc.sh
@@ -3,7 +3,7 @@
 # path:   /home/klassiker/.local/share/repos/dmenu/scripts/dmenu_calc.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/dmenu
-# date:   2023-01-31T19:01:22+0100
+# date:   2023-06-08T07:18:14+0200
 
 # get active window id
 window_id=$(xdotool getactivewindow)
@@ -13,12 +13,15 @@ clipboard=$(xsel -n -o -b)
 
 # use bc for calculations
 result=$(printf "%s\n" "$@" \
+    | sed 's/,/./g' \
     | bc -l
 )
 
 # format result and label
 [ -n "$result" ] \
-    && result=$(printf "%.4f\n" "$result") \
+    && result=$(printf "%.4f\n" "$result" \
+        | sed 's/\./,/g' \
+    ) \
     && label="$result ="
 
 menu() {
@@ -67,7 +70,7 @@ case $select in
             && "$0" &
         ;;
     "power costs")
-        "$0" "24*7*52/1000*0.40" &
+        "$0" "24*7*52/1000*0,40" &
         ;;
     [-+/*]*)
         "$0" "$result$select" &
