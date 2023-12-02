e910bb6a10148d607beaead503fca0dd6bf5baaf
mrdotx <klassiker@gmx.de>
2023-12-02 08:24:26 +0100

    fonts changed

--- config.def.h
+++ config.def.h
@@ -8,9 +8,9 @@ static int centered = 0;
 /* minimum width when centered */
 static int min_width = 250;
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static char font[]  = "DejaVuSansM Nerd Font Mono:pixelsize=16";
-static char font0[] = "DejaVu Sans:pixelsize=16";
-static char font1[] = "Noto Color Emoji:pixelsize=16";
+static char font[]  = "DejaVu Sans Mono:pixelsize=16";
+static char font0[] = "DejaVuSansM Nerd Font:pixelsize=16";
+static char font1[] = "Noto Color Emoji:pixelsize=14";
 static const char *fonts[] = {
 	font,
 	font0,
