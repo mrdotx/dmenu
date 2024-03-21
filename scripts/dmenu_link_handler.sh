45d2e7a8dd9d60da7decfeb02c848d90fb50434e
mrdotx <klassiker@gmx.de>
2024-03-21 07:45:29 +0100

    select download format and download on host added

--- scripts/dmenu_link_handler.sh
+++ scripts/dmenu_link_handler.sh
@@ -3,12 +3,25 @@
 # path:   /home/klassiker/.local/share/repos/dmenu/scripts/dmenu_link_handler.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/dmenu
-# date:   2024-03-17T19:01:05+0100
+# date:   2024-03-20T19:55:45+0100
 
 # i3 helper
 . dmenu_helper.sh
 
 title="link-handler"
+
+# helper functions
+download() {
+    host=$(printf "%s" "$2" | awk -F "$1" '{print $2}')
+    shift 2
+
+    [ -z "$host" ] \
+        && $TERMINAL -e terminal_wrapper.sh eval "$@" &
+
+    [ -n "$host" ] \
+        && dmenu_notify 2500 "$title" "$urls\ndownload on host: $host" \
+        && ssh -t "$host" "$@" &
+}
 
 case "$1" in
     --clipboard)
@@ -24,19 +37,20 @@ esac
     && exit 0
 
 dmenu_notify 0 "$title" "$urls"
-
 select=$(printf "%s\n" \
     "play video" \
     "play audio" \
     "add video to taskspooler" \
     "add audio to taskspooler" \
+    "select download format" \
     "download video" \
     "download audio" \
-    "select download format" \
     "download file" \
-    | dmenu -l 15 -c -bw 1 -r -i -p "$title »" \
+    "download video on m625q" \
+    "download audio on m625q" \
+    "download file on m625q" \
+    | dmenu -l 15 -c -bw 1 -i -p "$title »" \
 )
-
 dmenu_notify 1 "$title"
 
 [ -z "$select" ] \
@@ -59,20 +73,18 @@ case "$select" in
         tsp mpv --no-terminal --no-video --force-window \
             ytdl://"$urls" >/dev/null 2>&1
         ;;
-    "download video")
-        $TERMINAL -e terminal_wrapper.sh \
-            yt-dlp -ciw "$urls" &
-        ;;
-    "download audio")
-        $TERMINAL -e terminal_wrapper.sh \
-            yt-dlp -ciw -x --audio-format mp3 --audio-quality 0 "$urls" &
-        ;;
     "select download format")
         $TERMINAL -e terminal_wrapper.sh \
             yt-dlp -ciwf - "$urls" &
         ;;
-    "download file")
-        $TERMINAL -e terminal_wrapper.sh \
-            aria2c.sh "$urls" &
+    "download video"*)
+        download "on " "$select" "yt-dlp -ciw \"$urls\""
+        ;;
+    "download audio"*)
+        download "on " "$select" "yt-dlp -ciw \
+            -x --audio-format mp3 --audio-quality 0 \"$urls\""
+        ;;
+    "download file"*)
+        download "on " "$select" "aria2c.sh \"$urls\""
         ;;
 esac
