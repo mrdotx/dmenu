79936d246b80fc709a8e1f07a149cd9c730ff028
mrdotx <klassiker@gmx.de>
2024-06-20 07:14:21 +0200

    submenu for download added

--- scripts/dmenu_link_handler.sh
+++ scripts/dmenu_link_handler.sh
@@ -3,7 +3,7 @@
 # path:   /home/klassiker/.local/share/repos/dmenu/scripts/dmenu_link_handler.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/dmenu
-# date:   2024-03-23T08:05:55+0100
+# date:   2024-06-19T18:44:53+0200
 
 # i3 helper
 . dmenu_helper.sh
@@ -12,14 +12,16 @@ title="link-handler"
 
 # helper functions
 download() {
-    host=$(printf "%s" "$2" | awk -F "$1" '{print $2}')
-    shift 2
+    host=$(printf "%s" "$select" | awk -F "on " '{print $2}')
 
-    [ -z "$host" ] \
-        && $TERMINAL -e terminal_wrapper.sh eval "$*" &
-
-    [ -n "$host" ] \
-        && ssh -q "$host" "$* >/dev/null 2>&1 &"
+    case "$host" in
+        '')
+            $TERMINAL -e terminal_wrapper.sh eval "$*" &
+            ;;
+        *)
+            ssh -q "$host" "$* >/dev/null 2>&1 &"
+            ;;
+    esac
 }
 
 case "$1" in
@@ -37,18 +39,30 @@ esac
 
 dmenu_notify 0 "$title" "$urls"
 select=$(printf "%s\n" \
+    "download" \
     "play video" \
     "play audio" \
     "add video to taskspooler" \
     "add audio to taskspooler" \
-    "download audio/video file" \
-    "download file" \
-    "download file on m625q" \
-    "record video stream" \
-    "record video stream on m625q" \
-    "record audio stream" \
-    "record audio stream on m625q" \
-    | dmenu -l 15 -c -bw 1 -i -p "$title »" \
+        | dmenu -l 15 -c -bw 1 -i -p "$title »" \
+)
+
+[ -z "$select" ] \
+    && dmenu_notify 1 "$title" \
+    && exit 0
+
+[ "$select" = "download" ] \
+    && select=$(printf "%s\n" \
+        "select format" \
+        "video (best)" \
+        "video (ext)" \
+        "audio" \
+        "file" \
+        "video (best) on m625q" \
+        "video (ext) on m625q" \
+        "audio on m625q" \
+        "file on m625q" \
+            | dmenu -l 15 -c -bw 1 -i -p "$title »" \
 )
 dmenu_notify 1 "$title"
 
@@ -72,23 +86,25 @@ case "$select" in
         tsp mpv --no-terminal --no-video --force-window \
             ytdl://"$urls" >/dev/null 2>&1
         ;;
-    "download audio/video file")
+    "select format")
         $TERMINAL -e terminal_wrapper.sh \
-            yt-dlp --continue --ignore-errors --no-overwrites \
+            yt-dlp \
                 --format - "$urls" &
         ;;
-    "download file"*)
-        download "on " "$select" "aria2c.sh \"$urls\""
+    "video (best)"*)
+        download "yt-dlp \
+            \"$urls\""
         ;;
-    "record video stream"*)
-        download "on " "$select" "yt-dlp \
-            --continue --ignore-errors --no-overwrites \
-            --embed-thumbnail --embed-metadata \"$urls\""
+    "video (ext)"*)
+        download "yt-dlp \
+            --format-sort \"ext\" \"$urls\""
         ;;
-    "record audio stream"*)
-        download "on " "$select" "yt-dlp \
-            --continue --ignore-errors --no-overwrites \
+    "audio"*)
+        download "yt-dlp \
             --extract-audio --audio-format mp3 --audio-quality 0 \
             --embed-thumbnail --embed-metadata \"$urls\""
+        ;;
+    "file"*)
+        download "aria2c.sh \"$urls\""
         ;;
 esac
