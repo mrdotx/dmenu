76ce76484a433d3252645ce5a067487545255f52
mrdotx <klassiker@gmx.de>
2025-01-28 07:17:49 +0100

    menu entries updated and structural optimizations

--- scripts/dmenu_screenshot.sh
+++ scripts/dmenu_screenshot.sh
@@ -3,7 +3,7 @@
 # path:   /home/klassiker/.local/share/repos/dmenu/scripts/dmenu_screenshot.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/dmenu
-# date:   2024-12-16T08:01:24+0100
+# date:   2025-01-28T07:17:46+0100
 
 # source dmenu helper
 . _dmenu_helper.sh
@@ -11,25 +11,26 @@
 title="screenshot"
 
 # config
-screenshot_directory="$HOME/Desktop"
-screenshot_file="$screenshot_directory/$title-$(date +"%FT%T%z").png"
-screenshot_command="maim -Buq $screenshot_file"
+screenshot_directory="$HOME/Downloads"
+screenshot_file="$screenshot_directory/${title}_$(date +"%FT%T%z").png"
+screenshot_command="maim --quiet --hidecursor $screenshot_file"
 screenshot_preview="nsxiv --quiet --scale-mode w $screenshot_file"
 
 execute() {
-    screenshot_command="$screenshot_command $*"
-    $screenshot_command \
-        && $screenshot_preview &
+    # WORKAROUND: dmenu does not close fast enough
+    sleep .1
+
+    eval "$screenshot_command $* && $screenshot_preview &"
 }
 
 # menu
 select=$(printf "%s\n" \
-            "desktop" \
-            "window" \
-            "selection" \
-            "desktop --delay 5" \
-            "window --delay 5" \
-                | dmenu -l 5 -c -bw 1 -p "$title »" \
+    "desktop" \
+    "desktop --delay=5" \
+    "window --capturebackground" \
+    "window" \
+    "selection" \
+        | dmenu -l 5 -c -bw 1 -p "$title »" \
 )
 
 case $select in
@@ -37,13 +38,13 @@ case $select in
         execute "$select"
         ;;
     "window"*)
-        execute "$select -i $(xdotool getactivewindow)"
+        execute "$select --window=$(xdotool getactivewindow)"
         ;;
     "selection"*)
         dmenu_notify 2500 \
             "$title" \
-            "select an area or a window for the screenshot"
-        execute "$select -so"
+            "select a window or an area for the screenshot"
+        execute "$select --select"
         ;;
     *)
         exit 0
