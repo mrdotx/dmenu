564b794afdb4200ba4ab7850252607cf1acf13e3
mrdotx <klassiker@gmx.de>
2025-03-07 06:52:53 +0100

    set notify-send expire time form 0 to max int for permanent message

--- scripts/_dmenu_helper.sh
+++ scripts/_dmenu_helper.sh
@@ -3,13 +3,15 @@
 # path:   /home/klassiker/.local/share/repos/dmenu/scripts/_dmenu_helper.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/dmenu
-# date:   2024-12-16T08:01:04+0100
+# date:   2025-03-07T06:52:14+0100
 
 dmenu_notify() {
     # WORKAROUND: notifications are sometimes not displayed
     sleep .1
 
-    dmenu_notify_timer="$1"
+    [ "$1" -eq 0 ] \
+        && dmenu_notify_timer=2147483647 \
+        || dmenu_notify_timer="$1"
     dmenu_notify_title="$2 [dmenu]"
     dmenu_notify_message="$3"
 
