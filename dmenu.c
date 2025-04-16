25fcb4cf0193a7f8e13403b60d32cfd0c80479de
mrdotx <klassiker@gmx.de>
2025-04-16 06:35:17 +0200

    latest commit added

--- dmenu.c
+++ dmenu.c
@@ -129,7 +129,7 @@ cleanup(void)
 {
 	size_t i;
 
-	XUngrabKey(dpy, AnyKey, AnyModifier, root);
+	XUngrabKeyboard(dpy, CurrentTime);
 	for (i = 0; i < SchemeLast; i++)
 		free(scheme[i]);
 	for (i = 0; items && items[i].text; ++i)
